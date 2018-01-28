899023c83c13d033cdc71ae80afc13f8cd1afca9
George Loo <gloosg@yahoo.com.sg>
2018-01-28 18:07:41 +0800

    make 2D

--- worlds.go
+++ worlds.go
@@ -26,10 +26,57 @@ const (
 	screenheight = 400
 )
 
+
+type worldtype struct {
+	x, y float64
+	msg string 
+}
+
 var (
   mousedownState bool
+  city []worldtype 
+  city2 [][]worldtype
+
 )
 
+func InitProg() {
+
+	city = make([]worldtype,5)
+	city[0].x = 1
+	city[0].msg = "George"
+	city[1].x = 23
+	city[1].msg = "Linda"
+	city[4].msg = "Audrey"
+	city[3].msg = "foo"
+
+	i := 0
+	for i < 5 {
+		fmt.Print(city[i].x,",",city[i].msg," jibai \n")
+		i += 1
+	}
+
+	city2 = make([][]worldtype,10)
+	for i=0;i<10;i++ {
+    	city2[i] = make([]worldtype, 10)  // 
+	}
+	city2[0][0].msg = "gopher"
+	city2[9][0].msg = "codemotherfucker"
+	city2[5][5].msg = "Singapore"
+	city2[6][5].msg = "Screwed"
+	city2[6][5].msg = "Saved"
+	i = 0
+
+	for i < 10 {
+		j := 0
+		for j < 10 {
+			fmt.Print(i,",",j," = ",city2[i][j].msg,"/")
+			j += 1
+		}
+		fmt.Print("\n")
+		i += 1
+	}
+
+}
 
 func mouseLeftdown(screen *ebiten.Image) {
 	mx, my := ebiten.CursorPosition()
@@ -67,7 +114,11 @@ func update(screen *ebiten.Image) error {
 
 }
 
+
 func main() {
+
+
+	InitProg()
 
     scale := 1.0
     // Initialize Ebiten, and loop the update() function
